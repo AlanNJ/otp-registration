16043926c974858a8aade96f0a9e8984e0a39ef0
AlanNJ <alannpn75@gmail.com>
2022-03-23 18:54:00 +0530

    fixed minor bugs

--- controllers/otpVerification.js
+++ controllers/otpVerification.js
@@ -1,19 +1,31 @@
 const otp = require("../model/otp");
 const User = require("../model/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const otpVerification = async (req, res) => {
+	const number = Number(req.body.number);
 	const otpHolder = await otp.find({
-		number: req.body.number,
+		number: number,
 	});
 	const latestOtp = otpHolder[otpHolder.length - 1].otp;
 	if (req.body.otp === latestOtp) {
-		const number = Number(req.body.number);
 		const user = new User({ number });
+		const token = jwt.sign({ _id: user._id }, process.env.JWT, {
+			expiresIn: "7d",
+		});
 		const result = await user.save();
 		const deleteMany = await otp.deleteMany({
 			number: req.body.number,
 		});
-		return res.status(200).send("Login Successfull");
+		return res
+			.status(200)
+			.send({
+				message: "login successfull",
+				token: token,
+				data: user,
+				ok: true,
+			});
 	} else {
 		res.json("wrong otp");
 	}
