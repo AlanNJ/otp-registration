065cd9d9ba7d7092cac603bcec24a00a2660d54c
AlanNJ <alannpn75@gmail.com>
2022-03-25 23:19:22 +0530

    second-commit

--- controllers/otpVerification.js
+++ controllers/otpVerification.js
@@ -4,30 +4,32 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const otpVerification = async (req, res) => {
-	const number = Number(req.body.number);
-	const otpHolder = await otp.find({
-		number: number,
-	});
-	const latestOtp = otpHolder[otpHolder.length - 1].otp;
-	if (req.body.otp === latestOtp) {
-		const user = new User({ number });
-		const token = jwt.sign({ _id: user._id }, process.env.JWT, {
-			expiresIn: "7d",
+	try {
+		const number = Number(req.body.number);
+		const otpHolder = await otp.find({
+			number: number,
 		});
-		const result = await user.save();
-		const deleteMany = await otp.deleteMany({
-			number: req.body.number,
-		});
-		return res
-			.status(200)
-			.send({
+		const latestOtp = otpHolder[otpHolder.length - 1].otp;
+		if (req.body.otp === latestOtp) {
+			const user = new User({ number });
+			const token = jwt.sign({ _id: user._id }, process.env.JWT, {
+				expiresIn: "7d",
+			});
+			const result = await user.save();
+			const deleteMany = await otp.deleteMany({
+				number: req.body.number,
+			});
+			return res.status(200).send({
 				message: "login successfull",
 				token: token,
 				data: user,
 				ok: true,
 			});
-	} else {
-		res.json("wrong otp");
+		} else {
+			res.json("wrong otp");
+		}
+	} catch (err) {
+		console.log(err);
 	}
 };
 module.exports = otpVerification;
